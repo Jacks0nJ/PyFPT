7008df3dca76bef7834c1835168ec8cc78ac7abc
Jacks0nJ <joseph.jackson@port.ac.uk>
2022-03-04 15:47:02 +0000

    Just testing if I can commit the folder

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Stochastic_IS_CODE documentation master file, created by
+   sphinx-quickstart on Fri Mar  4 15:14:21 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Stochastic_IS_CODE's documentation!
+==============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- is_data_analysis.py
+++ is_data_analysis.py
@@ -2,7 +2,6 @@
 #  -*- coding: utf-8 -*-
 """
 Created on 28/02/22
-
 @author: jjackson
 """
 from timeit import default_timer as timer
@@ -109,13 +108,7 @@ def IS_simulation(phi_i, phi_end, V, V_dif, V_ddif, num_sims, bias, bins=50,
     w_values = w_values[sort_idx]
 
     # Checking if multipprocessing error occured, by looking at correlation
-    pearson_corr = np.corrcoef(sim_N_dist, np.log10(w_values))
-    pearson_corr = pearson_corr[0, 1]
-
-    if pearson_corr > -0.55:  # Data is uncorrelated
-        print(len(sim_N_dist))
-        print(sim_N_dist)
-        raise ValueError('Possible multiprocessing error occured, terminating')
+    multi_processing_error(sim_N_dist, w_values)
 
     # Truncating the data
     sim_N_dist, w_values =\
@@ -218,53 +211,11 @@ def data_points_pdf(Ns, ws, num_sub_samples, reconstruction,
         errors = errors_est[:, errors_est[0, :] > 0]/analytical_norm
 
         # Checking p-values if lognormal was used
-        p_values = np.zeros(num_bins)
-        p_values_theory = np.zeros(num_bins)
-        under_filled_bins = False
-        for i in range(len(p_values)):
-            if filled_bins[i] is True:
-                w = weights_in_bins[:, i]
-                log_w = np.log(w[w > 0])
-                _, p_values[i] = sci_stat.normaltest(log_w)
-                _, p_values_theory[i] =\
-                    sci_stat.normaltest(np.random.normal(0, 1, len(log_w)))
+        lognormality_check(bin_centres, weights_in_bins, filled_bins,
+                           num_bins)
 
-                if under_filled_bins is False and len(log_w) < 5000 and\
-                   i > int(len(p_values)/4):
-
-                    under_filled_bins = bin_centres_uncut[i]
-        p_values = p_values[filled_bins]
-        p_values_theory = p_values_theory[filled_bins]
-        if any(p_values) < 0.005:
-            print('Possibly not log normal distribution, see p-value plot')
-            print(p_values)
-            plt.errorbar(bin_centres, p_values, fmt='.', ms=7)
-            plt.hlines(0.005, np.min(bin_centres), np.max(bin_centres),
-                       color='k', linestyle='dashed',
-                       label='{0}'.format('0.5% threshold'), linewidth=2)
-
-            plt.yscale('log')
-            # plt.title('Data: p-values with bin centres')
-            plt.legend(fontsize=20)
-            plt.xlabel(r'$\mathcal{N}$', fontsize=20)
-            plt.ylabel('p-values', fontsize=20)
-
-            plt.show()
-            plt.clf()
-
-            plt.errorbar(bin_centres, p_values_theory, fmt='.')
-            plt.hlines(0.005, np.min(bin_centres), np.max(bin_centres),
-                       color='k', linestyle='dashed',
-                       label='{0}'.format('0.5% threshold'))
-            plt.yscale('log')
-            plt.title('Theoretical: p-values with bin centres')
-            plt.legend(fontsize=22)
-            plt.xlabel(r'$\mathcal{N}$', fontsize=22)
-            plt.ylabel('p-values', fontsize=22)
-            plt.show()
-            plt.clf()
     else:
-        raise ValueError('log_normal must be boolean')
+        raise ValueError('Not valid reconstrcution method')
 
     return bin_centres, heights, errors, num_sims_used, bins
 
@@ -283,3 +234,60 @@ def save_data_to_file(sim_N_dist, w_values, phi_i, num_sims, bias=0):
     # Saving to a directory for the language used
 
     data_pandas_raw.to_csv(raw_file_name)
+
+
+# This function tests if a multiprocessing error has occured. This is when the
+# data from the different cores becomes mixed, and the weights and N are not
+# correct
+def multi_processing_error(sim_N_dist, w_values):
+    # Checking if multipprocessing error occured, by looking at correlation
+    pearson_corr = np.corrcoef(sim_N_dist, np.log10(w_values))
+    pearson_corr = pearson_corr[0, 1]
+
+    if pearson_corr > -0.55:  # Data is uncorrelated
+        print('Possible multiprocessing error occured, terminating')
+
+
+def lognormality_check(bin_centres, weights_in_bins, filled_bins, num_bins):
+    # Checking p-values if lognormal was used
+    p_values = np.zeros(num_bins)
+    p_values_theory = np.zeros(num_bins)
+    for i in range(len(p_values)):
+        if filled_bins[i] is True:
+            w = weights_in_bins[:, i]
+            log_w = np.log(w[w > 0])
+            _, p_values[i] = sci_stat.normaltest(log_w)
+            _, p_values_theory[i] =\
+                sci_stat.normaltest(np.random.normal(0, 1, len(log_w)))
+
+    p_values = p_values[filled_bins]
+    p_values_theory = p_values_theory[filled_bins]
+    if any(p_values) < 0.005:
+        print('Possibly not log normal distribution, see p-value plot')
+        print(p_values)
+        plt.errorbar(bin_centres, p_values, fmt='.', ms=7)
+        plt.hlines(0.005, np.min(bin_centres), np.max(bin_centres),
+                   color='k', linestyle='dashed',
+                   label='{0}'.format('0.5% threshold'), linewidth=2)
+
+        plt.yscale('log')
+        # plt.title('Data: p-values with bin centres')
+        plt.legend(fontsize=20)
+        plt.xlabel(r'$\mathcal{N}$', fontsize=20)
+        plt.ylabel('p-values', fontsize=20)
+
+        plt.show()
+        plt.clf()
+
+        plt.errorbar(bin_centres, p_values_theory, fmt='.')
+        plt.hlines(0.005, np.min(bin_centres), np.max(bin_centres),
+                   color='k', linestyle='dashed',
+                   label='{0}'.format('0.5% threshold'))
+        plt.yscale('log')
+        plt.title('Theoretical: p-values with bin centres')
+        plt.legend(fontsize=22)
+        plt.xlabel(r'$\mathcal{N}$', fontsize=22)
+        plt.ylabel('p-values', fontsize=22)
+        plt.show()
+        plt.clf()
+    
